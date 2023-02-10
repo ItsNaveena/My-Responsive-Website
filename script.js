754571fd9a5a79bb0749bec8e7bffe12d5477cbd
Naveena <N.Naveena2255@gmail.com>
2023-02-10 20:49:26 +0530

    added more htmls

---
+++ script.js
@@ -0,0 +1,22 @@
+// let burger = document.getElementById('.burger');
+// let navbar = document.getElementById('.navbar');
+// let navbarList = document.getElementById('.navbarList');
+// let search = document.getElementById('.search');
+
+// burger.addEventListener('click', ()=>{
+
+//     search.classList.toggle('navListResponsive');
+//     navbarList.classList.toggle('navListResponsive');
+//     navbar.classList.toggle('navResponsive');
+
+// })
+
+
+let burger = document.getElementById('.burger');
+let togglebar = document.getElementById('.toggleList');
+
+burger.addEventListener('click', function(){
+
+       console.log('hii');
+    });
+    
